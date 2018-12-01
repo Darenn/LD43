3842459e05e9ed0a6195d9e014b6194071f67d18
Darenn Keller <keller.darenn@gmail.com>
2018-12-01 17:34:37 +0100

    Added standard assets, aim example, outline script. The player can move, and grab one object (the object is outlined when the player look it).

---
+++ LD43/Assets/Scripts/GrabbableObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Outline))]
+public class GrabbableObject : MonoBehaviour
+{
+    public GameObject ObjectPrefabToGiveOnGrab;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    public GameObject getObjectToGrab()
+    {
+        return Instantiate(ObjectPrefabToGiveOnGrab);
+    }
+}
---
+++ LD43/Assets/Scripts/RaycastTarget.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastTarget : MonoBehaviour
+{
+
+    public float RaycastRange;
+    public Transform ObjectGrabbedPosition;
+
+    private Camera fpsCam;
+    private GameObject current_target;
+
+    private GameObject objectGrabbed = null;
+
+    void Start () {
+	    fpsCam = GetComponentInParent<Camera>();
+    }
+	
+	// Update is called once per frame
+    void Update()
+    {
+        Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+        RaycastHit hit;
+        GameObject newTarget = null;
+        if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, RaycastRange))
+        {
+            if (hit.transform.GetComponent<GrabbableObject>() != null)
+            {
+                newTarget = hit.transform.gameObject;
+            }
+        }
+
+        if (newTarget != current_target)
+        {
+            if (current_target != null) 
+                current_target.GetComponent<Outline>().enabled = false;
+            current_target = newTarget;
+            if (newTarget != null)
+                current_target.GetComponent<Outline>().enabled = true;
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            if (current_target != null)
+            {
+                objectGrabbed = current_target.GetComponent<GrabbableObject>().getObjectToGrab();
+                objectGrabbed.transform.parent = transform;
+                objectGrabbed.transform.position = ObjectGrabbedPosition.position;
+                Destroy(current_target);
+                current_target = null;
+            }
+        }
+    }
+}
