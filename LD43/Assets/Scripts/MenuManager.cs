1423c59b3f6c17aab2cf3c65fffba15d0412af78
Darenn Keller <keller.darenn@gmail.com>
2018-12-03 16:14:36 +0100

    Starting animations, menu, etc...

---
+++ LD43/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	[SerializeField] private Animator m_animator;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	void PressPlay() {
+		m_animator.SetTrigger("PressPlay");
+	}
+}
